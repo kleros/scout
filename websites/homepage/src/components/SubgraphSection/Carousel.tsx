f4bea23737afe0badf8fdf05dcd4f9e5bb3ce05d
Harman-singh-waraich <Waraichharman68@gmail.com>
2024-05-20 15:04:49 +0530

    fix: carousel-gql-query-style

--- websites/homepage/src/components/SubgraphSection/Carousel.tsx
+++ websites/homepage/src/components/SubgraphSection/Carousel.tsx
@@ -218,7 +218,7 @@ const components = [
     description:
       'Kleros’ Address tags provides crucial contract insights BEFORE interaction.',
     query:
-      '{\n  lregistry(id: "0x66260c69d03837016d88c9877e61e08ef74c59f2") {    numberOfRegistered\n    items {\n      status\n      metadata : {\n    props {\n        label\n        value\n      }\n     }\n    }\n  }\n}',
+      '{\n  lregistry(id: "0x66260c69d03837016d88c9877e61e08ef74c59f2") {\n      numberOfRegistered\n      items {\n        status\n        metadata {\n          props {\n          label\n          value\n          }\n         }\n     }\n  }\n}',
     imageUrl: EtherscanImg,
   },
   {
@@ -226,7 +226,7 @@ const components = [
     description:
       'Secure users from front-end DNS attacks, phishing, etc. with Kleros’ Contract to Domain Name mappings.',
     query:
-      '{\n  lregistry(id: "0x957a53a994860be4750810131d9c876b2f52d6e1") {   numberOfRegistered\n    items {\n      status\n      metadata : {\n    props {\n        label\n        value\n      }\n    }\n    }\n  }\n}',
+      '{\n  lregistry(id: "0x957a53a994860be4750810131d9c876b2f52d6e1") {\n      numberOfRegistered\n      items {\n        status\n        metadata {\n          props {\n          label\n          value\n          }\n        }\n     }\n  }\n}',
     imageUrl: MetamaskPopup,
   },
   {
@@ -234,7 +234,7 @@ const components = [
     description:
       'Use Kleros’ Tokens, the oldest & still functioning TCR in web3.',
     query:
-      '{\n  lregistry(id: "0xee1502e29795ef6c2d60f8d7120596abe3bad990") {    numberOfRegistered\n    items {\n      status\n      metadata : {\n    props {\n        label\n        value\n      }\n    }\n    }\n  }\n}',
+      '{\n  lregistry(id: "0xee1502e29795ef6c2d60f8d7120596abe3bad990") {\n      numberOfRegistered\n      items {\n        status\n        metadata {\n          props {\n          label\n          value\n          }\n        }\n     }\n  }\n}',
     imageUrl: KlerosTokensImg,
   },
 ]
