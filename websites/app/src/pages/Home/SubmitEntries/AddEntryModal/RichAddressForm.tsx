a3aff82ff60cde6a3879208a9f0d5626c8c0bb26
Shubham Parkhi <shubhamparkhi10@gmail.com>
2024-06-24 14:59:55 +0530

    fix(web): Style changes in modal

--- websites/app/src/pages/Home/SubmitEntries/AddEntryModal/RichAddressForm.tsx
+++ websites/app/src/pages/Home/SubmitEntries/AddEntryModal/RichAddressForm.tsx
@@ -18,19 +18,15 @@ const StyledNetworkSelect = styled(Select)`
   width: 200px;
 
   > div {
-    background-color: #525252;
+    background: #262626;
     border: none;
     border-radius: 12px 12px 0 0;
 
     > input {
       color: #fff;
       padding: 12px;
-      background-color: #525252;
+      background: rgba(255, 255, 255, 0.15);
       border-radius: 12px;
-
-      :hover {
-        background-color: #525252;
-      }
     }
   }
   * div {
@@ -42,7 +38,7 @@ const StyledNetworkSelect = styled(Select)`
 export const StyledAddressInput = styled.input`
   display: flex;
   width: 100%;
-  background: #525252;
+  background: rgba(255, 255, 255, 0.15);
   padding: 8px 12px;
   outline: none;
   border: none;
