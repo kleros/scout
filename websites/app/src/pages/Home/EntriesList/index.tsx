2f21f2823cbafd4055dc46a5e86fd6bff9b2f206
Shubham Parkhi <shubhamparkhi10@gmail.com>
2024-07-04 23:54:27 +0530

    fix(web): cleaner code

--- websites/app/src/pages/Home/EntriesList/index.tsx
+++ websites/app/src/pages/Home/EntriesList/index.tsx
@@ -1,15 +1,11 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import styled, { css } from 'styled-components'
 import { landscapeStyle } from 'styles/landscapeStyle'
-import { GraphItem, registryMap } from 'utils/fetchItems'
+import { GraphItem } from 'utils/fetchItems'
 import Entry from './Entry'
 import { ITEMS_PER_PAGE } from 'pages/Home'
 
-interface EntriesContainerProps {
-  isCDN: boolean;
-}
-
-const EntriesContainer = styled.div<EntriesContainerProps>`
+const EntriesContainer = styled.div`
   width: 80%;
   display: grid;
   gap: 20px 40px;
@@ -29,12 +25,8 @@ interface IEntriesList {
 }
 
 const EntriesList: React.FC<IEntriesList> = ({ searchData }) => {
-  const isCDN = useMemo(() => {
-    return searchData.some(item => item.registryAddress === registryMap['CDN']);
-  }, [searchData]);
-
   return (
-    <EntriesContainer isCDN={isCDN}>
+    <EntriesContainer>
       {searchData.slice(0, ITEMS_PER_PAGE).map((item) => (
         <Entry key={item.id} item={item} />
       ))}
