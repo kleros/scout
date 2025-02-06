38ada14d8fd6a5235d5a1bfb0d300e2697687d76
kemuru <102478601+kemuru@users.noreply.github.com>
2025-02-06 14:35:17 +0100

    chore: change to thegraph free endpoint

--- websites/app/src/consts/index.tsx
+++ websites/app/src/consts/index.tsx
@@ -1,1 +1,1 @@
-export const SUBGRAPH_GNOSIS_ENDPOINT = "https://api.goldsky.com/api/public/project_cm5y7hx91t6zd01vzfnfchtf9/subgraphs/legacy-curate-gnosis/latest/gn"
+export const SUBGRAPH_GNOSIS_ENDPOINT = "https://api.studio.thegraph.com/query/61738/legacy-curate-gnosis/v0.1.1"
