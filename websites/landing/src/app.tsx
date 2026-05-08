a63528b2cb6b908eb1eeaf83ddb8078db2ea1f80
kemuru <102478601+kemuru@users.noreply.github.com>
2026-05-08 16:01:04 +0200

    fix: layout

--- websites/landing/src/app.tsx
+++ websites/landing/src/app.tsx
@@ -1,11 +1,7 @@
-import React, { useRef } from 'react'
-import styled from 'styled-components'
-import 'overlayscrollbars/styles/overlayscrollbars.css'
+import React from 'react'
 import 'react-loading-skeleton/dist/skeleton.css'
 import { Route, Routes } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { OverlayScrollbarsComponent } from 'overlayscrollbars-react'
-import { OverlayScrollContext } from 'context/OverlayScrollContext'
 import StyledComponentsProvider from 'context/StyledComponentsProvider'
 import Layout from './layout'
 import Home from 'pages/Home'
@@ -13,37 +9,23 @@ import ForUsers from './pages/ForUsers'
 import ForBuilders from './pages/ForBuilders'
 import TermsOfService from './pages/TermsOfService'
 
-const StyledOverlayScrollbarsComponent = styled(OverlayScrollbarsComponent)`
-  height: 100vh;
-  width: 100vw;
-`
-
 const queryClient = new QueryClient()
 
 const App: React.FC = () => {
-  const containerRef = useRef(null)
-
   return (
-    <OverlayScrollContext.Provider value={containerRef}>
-      <StyledOverlayScrollbarsComponent
-        ref={containerRef}
-        options={{ showNativeOverlaidScrollbars: true }}
-      >
-        <StyledComponentsProvider>
-          <QueryClientProvider client={queryClient}>
-            <Routes>
-              <Route path="/" element={<Layout />}>
-                <Route index element={<Home />} />
-                <Route path="for-users/*" element={<ForUsers />} />
-                <Route path="for-builders/*" element={<ForBuilders />} />
-                <Route path="terms-of-service" element={<TermsOfService />} />
-                <Route path="*" element={<h1>404 Not Found</h1>} />
-              </Route>
-            </Routes>
-          </QueryClientProvider>
-        </StyledComponentsProvider>
-      </StyledOverlayScrollbarsComponent>
-    </OverlayScrollContext.Provider>
+    <StyledComponentsProvider>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="for-users/*" element={<ForUsers />} />
+            <Route path="for-builders/*" element={<ForBuilders />} />
+            <Route path="terms-of-service" element={<TermsOfService />} />
+            <Route path="*" element={<h1>404 Not Found</h1>} />
+          </Route>
+        </Routes>
+      </QueryClientProvider>
+    </StyledComponentsProvider>
   )
 }
 
