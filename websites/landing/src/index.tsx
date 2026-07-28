12049e55c00bc7af9dd0a3a61048624275be34da
kemuru <102478601+kemuru@users.noreply.github.com>
2026-07-28 17:22:43 +0200

    chore: remove defunct galxe integration

--- websites/landing/src/index.tsx
+++ websites/landing/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app'
-import Modal from 'react-modal'
 import { BrowserRouter } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 
@@ -12,7 +11,6 @@ declare global {
 }
 
 const container = document.getElementById('app')
-Modal.setAppElement(container!)
 const root = createRoot(container!)
 root.render(
   <React.StrictMode>
